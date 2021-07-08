0422ff91cc5549320458b346d22a6e42979771bd
Rafa Soler <rafasoler10@gmail.com>
2021-07-08 12:39:02 +0200

    Show out of stock msg when all variations are out

--- store.alisondarwin.com/wp-content/themes/generatepress-child/functions.php
+++ store.alisondarwin.com/wp-content/themes/generatepress-child/functions.php
@@ -273,6 +273,31 @@ add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loo
 add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_link_open', 20 );
 
 
+// Override a la función para mostrar "Agotado" cuando todas las variaciones están sin stock
+function woocommerce_single_variation() {
+	global $product; 
+	// TODO
+	$classes =  'woocommerce-variation single_variation';
+	if ( is_variable_product_out_of_stock($product) ) {
+		$classes .= ' sold-out';
+	}
+	echo "<div class='$classes'></div>";
+}
+
+function is_variable_product_out_of_stock( $product ) {
+    $children_count = 0;
+    $variation_ids  = $product->get_visible_children();
+
+    foreach( $variation_ids as $variation_id ) {
+        $variation = wc_get_product($variation_id);
+            
+        if( ! $variation->is_in_stock() ) {
+            $children_count++;
+        }
+    }
+    
+    return (count($variation_ids) == $children_count);
+}
 
 
 /*********** AMP ***********/
